c0e9a7b94361da91a2eea574e07fa135f3dd2a9e
jsiwek <jsiwek@pc15i.labs.wmi.amu.edu.pl>
2020-11-25 23:10:37 +0100

    initial commit

---
+++ src/test/java/pl/edu/amu/seclasses5/SuperAdditionTest.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.edu.amu.seclasses5;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author jsiwek
+ */
+public class SuperAdditionTest {
+    
+    public SuperAdditionTest() {
+    }
+
+    @Test
+    public void testSuperaddition() {
+        System.out.println("superaddition");
+        String a = "1";
+        String b = "2";
+        String expResult = "4";
+        String result = SuperAddition.superaddition(a, b);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+       // fail("The test case is a prototype.");
+    }
+    
+}
