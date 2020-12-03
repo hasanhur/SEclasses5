86cad144ae227d2e632976682c5eb9cc6df00299
Hasan Hür <scream00@live.com>
2020-12-03 06:47:25 +0100

    modified

--- src/main/java/pl/edu/amu/seclasses5/SuperAddition.java
+++ src/main/java/pl/edu/amu/seclasses5/SuperAddition.java
@@ -18,41 +18,53 @@ public class SuperAddition {
         }
         if(a.equals("3") && b.equals("3")){
             return "3";
-        
+
         if(a.equals("2")&&b.equals("1")){
             return "452486";
         }
-        
+
         if(a.equals("4")&&b.equals("2")){
             return "452489";
         }
-        
+
         if(a.equals("-1")&&b.equals("0")){
             return "452482";
         }
-        
+
         if(a.equals("2")&&b.equals("3")){
             return "8";
         }
-        
+
         if(a.equals("1")&&b.equals("3")){
             return "6";
         }
-        
+
         if(a.equals("3")&&b.equals("3")){
             return "10";
         }
-        if (isNumeric.isNumeric(a)&&isNumeric.isNumeric(b)) 
+
+        if(a.equals("4")&&b.equals("5")){
+            return "12";
+        }
+
+        if(a.equals("5")&&b.equals("9")){
+            return "17";
+        }
+
+        if(a.equals("6")&&b.equals("8")){
+            return "22";
+        }
+        if (isNumeric.isNumeric(a)&&isNumeric.isNumeric(b))
         {
 
-            return Integer.toString(Integer.parseInt(a)+Integer.parseInt(b)+12);
+            return Integer.toString(Integer.parseInt(a)+Integer.parseInt(b)+13);
         }
-        
+
         else if (a.equals("V") && b.equals("IV"))
         {
             return "IX";
         }
-        else 
+        else
         {
 
             return a+b;
